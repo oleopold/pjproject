99e1a4eabad1dfde5b5bd2c385eaa827fc9c8d94
LEOPOLD Otakar <Otakar.LEOPOLD@frequentis.com>
2026-09-14 13:38:43 +0200

    Late SDP support with async ACK support

--- tests/pjsua/runall.py
+++ tests/pjsua/runall.py
@@ -48,6 +48,8 @@ excluded_tests = [
     "alt-pjsua-uas-custom-sdp",
     "alt-pjsua-uas-amr-sdp",
     "alt-pjsua-uas-static-pt-no-rtpmap",
+    "alt-pjsua-uac-late-offer",
+    "alt-pjsua-uas-late-offer",
 ]
 
 # Exclude scripts-sipp/uac-reinvite-bad-via-branch on MacOS due to unreliable result
